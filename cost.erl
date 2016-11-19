c5ae4c1193860b8348766e69c5b0fe4916120034
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2016-11-19 07:31:04 +0200

    a module containing financial estimate functions added

---
+++ cost.erl
@@ -0,0 +1,12 @@
+%
+% This module contains functions that estimate diferent financial
+% properties of the model like cost of construction and potential revenue
+% 
+
+-module(cost).
+-export([estimate_profit/1
+		]).
+
+
+
+estimate_profit(Plan) -> 0.
