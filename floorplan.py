b04f191b51af61d09f628915575dfd83f92e2193
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2017-01-03 17:18:29 +0200

    a new module for floorplans added

---
+++ floorplan.py
@@ -0,0 +1,21 @@
+#
+# This module contains floorplan class
+#	the floorplan assumes rectangular shape of the building and
+#	rectangular shape of all units. Please look at the diagram ...
+#	describing all parameters of the layout
+#
+
+
+import numpy as np
+
+
+class FloorPlan():
+	def __init__(self,L1,L2):
+		self.L1 = L1
+		self.L2 = L2
+
+
+	def copy(self):
+		fp = FloorPlan(self.L1, self.L2)
+
+		return fp
